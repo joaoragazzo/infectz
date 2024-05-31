4d00db01e5dbbba6c0618ac0d3d19afa076b5609
joaoragazzo <joao_ragazzo@hotmail.com>
2024-05-31 16:02:16 -0300

    fix: fixing config in database

--- app/config.py
+++ app/config.py
@@ -7,5 +7,5 @@ class Config:
     MERCADO_PAGO_SDK_KEY = 'TEST-4895180949334138-052121-b1da13509c6cdc1959dc03a74cb2c0ec-1566496776'
     STEAM_API_KEY = "E41C0983D3EDF0DCD85BD43D0B0E5FCB"
     SERVER_SITE_URL = "https://infectz.0x6a70.com"  # Obs: Não colocar a '/' no final
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://hg0x6a20_infectz:kTfySx4CyjH0@localhost/hg0x6a20_infectz"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://hg0x6a20_infectz:P8Cx3xbDqLSC0SbJTu@localhost/hg0x6a20_infectz"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
