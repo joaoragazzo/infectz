b4a2f94519c3e487eb213f980ebe2642e1a2df42
joaoragazzo <joao_ragazzo@hotmail.com>
2024-06-28 19:17:29 -0300

    fix: trying to fix a unknown bug

--- run.py
+++ run.py
@@ -2,14 +2,14 @@ from app import create_app
 import logging
 from logging.handlers import RotatingFileHandler
 
-app = create_app()
-
-
 handler = RotatingFileHandler('error.log', maxBytes=10000, backupCount=3)
 handler.setLevel(logging.ERROR)
 formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
 handler.setFormatter(formatter)
+
+
+app = create_app()
 app.logger.addHandler(handler)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
