0cea8131390d4b972d7606a88c79d6a551bdf98b
joaoragazzo <joao_ragazzo@hotmail.com>
2024-06-28 19:41:55 -0300

    fix: fixing some small issue on cart

--- app/services/cart.py
+++ app/services/cart.py
@@ -2,13 +2,13 @@ from app.models import db, Cart, Item
 
 
 def check_item_existence(item_id: int) -> bool:
-    item_existence = Item.query.filter_by(id=item_id).query().first()
+    item_existence = Item.query.filter_by(id=item_id).first()
     return item_existence
 
 
 def add_cart_item(item_id: int, user_id: int) -> None:
     if not check_item_existence(item_id):
-        pass
+        raise Exception("This item does not exists in our database.")
 
     try:
         db.session.add(Cart(item_id, user_id))
@@ -19,7 +19,7 @@ def add_cart_item(item_id: int, user_id: int) -> None:
 
 def remove_cart_item(item_id: int, user_id: int) -> None:
     if not check_item_existence(item_id):
-        pass
+        raise Exception("This item does not exists in our database.")
 
     item_to_remove = Item.query.filter_by(item_id=item_id, user_id=user_id).first()
 
