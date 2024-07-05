697eba27d7c796aeacc2e296a5fbd1cc9bcf1ee1
joaoragazzo <joao_ragazzo@hotmail.com>
2024-07-05 20:09:02 -0300

    feat: adapting new models for the new database schema

--- app/models.py
+++ app/models.py
@@ -1,31 +1,11 @@
 from . import db
 
 
-class Item(db.Model):
+class Clan(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    tag = db.Column(db.String(10), nullable=False)
     name = db.Column(db.String(255), nullable=False)
-    description = db.Column(db.Text, nullable=False)
-    price = db.Column(db.Float, nullable=False)
-    price_off = db.Column(db.Float, nullable=True, default=0.0)
-    image_url = db.Column(db.String(255), nullable=True)
-
-    category = db.relationship('Category', backref='items', lazy=True)
-
-
-class Inventory(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.BigInteger, db.ForeignKey('user.steam64id'), nullable=False)
-    item_id = db.Column(db.Integer, db.ForeignKey('item.id'), nullable=False)
-    redeemed = db.Column(db.Boolean, nullable=False, default=False)
-    date_bought = db.Column(db.DateTime, nullable=False)
-
-    item = db.relationship('Item', backref='inventory_items', lazy=True)
-
-
-class Category(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(255), nullable=False)
+    leader_id = db.Column(db.BigInteger, db.ForeignKey('user.steam64id'), nullable=True)
 
 
 class User(db.Model):
@@ -34,9 +14,58 @@ class User(db.Model):
     shop_notifications = db.Column(db.Integer, nullable=True, default=0)
     config_notifications = db.Column(db.Integer, nullable=True, default=0)
     cart_notifications = db.Column(db.Integer, nullable=True, default=0)
+    first_login = db.Column(db.DateTime, nullable=False)
+    last_login = db.Column(db.DateTime, nullable=False)
+    clan_id = db.Column(db.Integer, db.ForeignKey('clan.id'), nullable=True)
+    role = db.Column(db.Enum('none', 'member', 'sub_leader', 'leader'), default='none', nullable=False)
 
+    clan = db.relationship('Clan', backref='members')
     inventory = db.relationship('Inventory', backref='user', lazy=True)
     cart = db.relationship('Cart', backref='user', lazy=True)
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255), nullable=False)
+    active = db.Column(db.Boolean, default=True)
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    price_off = db.Column(db.Float, default=0.0)
+    image_url = db.Column(db.String(255), nullable=True)
+    active = db.Column(db.Boolean, default=True)
+
+    category = db.relationship('Category', backref='items', lazy=True)
+
+
+class Payment(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    mercado_pago_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.BigInteger, db.ForeignKey('user.steam64id'), nullable=False)
+    item_id = db.Column(db.Integer, db.ForeignKey('item.id'), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+    payment_confirmed = db.Column(db.Boolean, default=False)
+
+    user = db.relationship('User', backref='payments', lazy=True)
+    item = db.relationship('Item', backref='payments', lazy=True)
+
+
+class Inventory(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.BigInteger, db.ForeignKey('user.steam64id'), nullable=False)
+    item_id = db.Column(db.Integer, db.ForeignKey('item.id'), nullable=False)
+    redeemed = db.Column(db.Boolean, default=False)
+    payment_id = db.Column(db.Integer, db.ForeignKey('payment.id'), nullable=False)
+    date_bought = db.Column(db.DateTime, nullable=False)
+
+    user = db.relationship('User', backref='inventory_items', lazy=True)
+    item = db.relationship('Item', backref='inventory_items', lazy=True)
+    payment = db.relationship('Payment', backref='inventory_items', lazy=True)
 
 
 class Cart(db.Model):
@@ -44,4 +73,5 @@ class Cart(db.Model):
     user_id = db.Column(db.BigInteger, db.ForeignKey('user.steam64id'), nullable=False)
     item_id = db.Column(db.Integer, db.ForeignKey('item.id'), nullable=False)
 
+    user = db.relationship('User', backref='cart_items', lazy=True)
     item = db.relationship('Item', backref='cart_items', lazy=True)
