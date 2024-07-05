4b0056bfeed97951cdd35e30e463e8c3e2981680
joaoragazzo <joao_ragazzo@hotmail.com>
2024-07-05 10:33:50 -0300

    feat: adding new columns for database

--- database/tables.sql
+++ database/tables.sql
@@ -1,21 +1,32 @@
 -- noinspection SqlNoDataSourceInspectionForFile
 -- noinspection SqlDialectInspectionForFile
 
+CREATE TABLE `clan` (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    tag VARCHAR(10) NOT NULL,
+    name VARCHAR(255) NOT NULL
+  );
+
 CREATE TABLE `user` (
     steam64id BIGINT(20) UNSIGNED PRIMARY KEY,
     inventory_notifications INT(11) DEFAULT 0,
     shop_notifications INT(11) DEFAULT 0,
     config_notifications INT(11) DEFAULT 0,
-    cart_notifications INT(11) DEFAULT 0
+    cart_notifications INT(11) DEFAULT 0,
+    first_login DATETIME NOT NULL,
+    last_login DATETIME NOT NULL,
+    clan_id INT(11),
+    role ENUM('none', 'member', 'sub_leader', 'leader') DEFAULT 'none',
+    FOREIGN KEY (clan_id) REFERENCES clan(id)
 );
 
-CREATE TABLE `category` (
+CREATE TABLE category (
     id INT(11) AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     active BOOLEAN DEFAULT TRUE
 );
 
-CREATE TABLE `item` (
+CREATE TABLE item (
     id INT(11) AUTO_INCREMENT PRIMARY KEY,
     category_id INT(11) NOT NULL,
     name VARCHAR(255) NOT NULL,
@@ -24,34 +35,35 @@ CREATE TABLE `item` (
     price_off FLOAT DEFAULT 0.0,
     image_url VARCHAR(255) NOT NULL,
     active BOOLEAN DEFAULT TRUE,
-    FOREIGN KEY (category_id) REFERENCES `category`(id)
+    FOREIGN KEY (category_id) REFERENCES category(id)
 );
 
-CREATE TABLE `inventory` (
-    id INT(11) AUTO_INCREMENT PRIMARY KEY,
-    user_id BIGINT(20) UNSIGNED NOT NULL,
-    item_id INT(11) NOT NULL,
-    redeemed BOOLEAN DEFAULT FALSE,
-    date_bought DATETIME NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES `user`(steam64id),
-    FOREIGN KEY (item_id) REFERENCES `item`(id)
-);
-
-CREATE TABLE `cart` (
-    id INT(11) AUTO_INCREMENT PRIMARY KEY,
-    user_id BIGINT(20) UNSIGNED NOT NULL,
-    item_id INT(11) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES `user`(steam64id),
-    FOREIGN KEY (item_id) REFERENCES `item`(id)
-);
-
-CREATE TABLE `payment` (
+CREATE TABLE payment (
     id INT(11) AUTO_INCREMENT PRIMARY KEY,
     mercado_pago_id INT(11) NOT NULL,
     user_id BIGINT(20) UNSIGNED NOT NULL,
     item_id INT(11) NOT NULL,
     created_at DATETIME NOT NULL,
     payment_confirmed BOOLEAN DEFAULT FALSE,
-    FOREIGN KEY (user_id) REFERENCES `user`(steam64id),
-    FOREIGN KEY (item_id) REFERENCES `item`(id)
+    FOREIGN KEY (user_id) REFERENCES user(steam64id),
+    FOREIGN KEY (item_id) REFERENCES item(id)
+);
+
+CREATE TABLE inventory (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT(20) UNSIGNED NOT NULL,
+    item_id INT(11) NOT NULL,
+    redeemed BOOLEAN DEFAULT FALSE,
+    payment_id INT(11) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(steam64id),
+    FOREIGN KEY (item_id) REFERENCES item(id),
+    FOREIGN KEY (payment_id) REFERENCES payment(id)
+);
+
+CREATE TABLE cart (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT(20) UNSIGNED NOT NULL,
+    item_id INT(11) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(steam64id),
+    FOREIGN KEY (item_id) REFERENCES item(id)
 );
