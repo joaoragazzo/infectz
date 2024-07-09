529891e8b55d840a2207eba4939dadc19763faa2
joaoragazzo <joao_ragazzo@hotmail.com>
2024-07-09 13:20:25 -0300

    feat: increasing the debug level

--- run.py
+++ run.py
@@ -2,14 +2,15 @@ from app import create_app
 import logging
 from logging.handlers import RotatingFileHandler
 
-handler = RotatingFileHandler('error.log', maxBytes=10000, backupCount=3)
-handler.setLevel(logging.ERROR)
+handler = RotatingFileHandler('error.log', maxBytes=100000, backupCount=3)
+handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
 handler.setFormatter(formatter)
 
 
 app = create_app()
 app.logger.addHandler(handler)
+app.logger.setLevel(logging.DEBUG)
 
 if __name__ == '__main__':
     app.run()
