cd70c820f3b6a42aadf29c71cf26c04d1c314927
joaoragazzo <joao_ragazzo@hotmail.com>
2024-07-09 13:59:01 -0300

    feat: increasing the debug level

--- app/services/user.py
+++ app/services/user.py
@@ -1,18 +1,22 @@
 from flask.sessions import SessionMixin
 from app.models import User
+from run import logger
 
 
+logger.debug("userservice -> get_user_by_session")
 def get_user_by_session(session: SessionMixin) -> User:
-    return User.query.filter_by(id=session['user_id']).first()
+    return User.query.filter_by(steam64id=session['steam64id']).first()
 
 
+logger.debug("userservice -> get_user_by_steam64id")
 def get_user_by_steam64id(steam64id: int | str) -> User:
     if type(steam64id) is str:
         steam64id = int(steam64id)
 
-    return User.query.filter_by(id=steam64id).first()
+    return User.query.filter_by(steam64id=steam64id).first()
 
 
+logger.debug("userservice -> user_exists")
 def user_exists(steam64id: int | str) -> bool:
     if type(steam64id) is str:
         steam64id = int(steam64id)
