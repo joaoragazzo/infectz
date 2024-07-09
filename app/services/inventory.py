d34d16cf4c81ba2c71ca3e8ea8eaaa142eb97bfd
joaoragazzo <joao_ragazzo@hotmail.com>
2024-07-09 14:34:24 -0300

    feat: increasing the debug level

--- app/services/inventory.py
+++ app/services/inventory.py
@@ -3,5 +3,5 @@ from app.models import Inventory
 from app import db
 
 
-def get_inventory(session: SessionMixin) -> list[Inventory]:
+def get_inventory(session: SessionMixin):
     return Inventory.query.filter_by(user_id=session['steam64id']).all()
